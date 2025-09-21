9ea0ffa3fe825d8fa32ffc1a5acdd698a99024a2
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-09-21 08:51:11 +0300

    fi

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-        include: ['bidi-js', 'jspdf', 'jspdf-autotable'], // تأكد من تضمين bidi-js
-
+    
     exclude: ['lucide-react'],
   },
 });
