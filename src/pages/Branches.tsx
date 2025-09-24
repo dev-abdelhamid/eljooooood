e3402e579571c8f6a73bd3d8082beb07f00d3a30
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-09-24 19:33:34 +0300

    sec333

--- src/pages/Branches.tsx
+++ src/pages/Branches.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useReducer, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useReducer, useRef , useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useAuth } from '../contexts/AuthContext';
