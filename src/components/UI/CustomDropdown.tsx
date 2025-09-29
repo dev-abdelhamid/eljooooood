9776efacab1dfac5faa306e96872d86ea724bc38
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-09-29 14:47:48 +0300

    sec44

--- src/components/UI/CustomDropdown.tsx
+++ src/components/UI/CustomDropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, memo } from 'react';
-import { useLanguage } from '../contexts/LanguageContext';
+import { useLanguage } from '../../contexts/LanguageContext';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ChevronDown } from 'lucide-react';
 
