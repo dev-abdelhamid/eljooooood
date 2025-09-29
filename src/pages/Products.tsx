76488901aa1a6c0a1723a53a5a837010a9b217ea
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-09-29 13:26:40 +0300

    sec44

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -117,6 +117,7 @@ const CustomDropdown = ({
       <AnimatePresence>
         {isOpen && (
           <motion.div
+            onClick={(e) => e.stopPropagation()}
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
