133a22324de2dfcf2f8e5aa5df05d36acec225de
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-01 16:24:05 +0300

    one

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useCallback , useRef  } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useAuth } from '../contexts/AuthContext';
