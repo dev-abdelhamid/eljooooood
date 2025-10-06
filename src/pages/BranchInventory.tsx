ad3aded2b0a26f086b473fae862172f0403ac887
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-06 18:14:05 +0300

    one

--- src/pages/BranchInventory.tsx
+++ src/pages/BranchInventory.tsx
@@ -4,7 +4,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { useSocket } from '../contexts/SocketContext';
 import { useNotifications } from '../contexts/NotificationContext';
 import { returnsAPI, ordersAPI, inventoryAPI } from '../services/api';
-import { Package, AlertCircle, Search, RefreshCw, Edit2, Trash2, X, Plus, Eye } from 'lucide-react';
+import { Package, AlertCircle, Search, RefreshCw, Edit2, Trash2, X, Plus, Eye , ChevronDown  } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { toast } from 'react-toastify';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
