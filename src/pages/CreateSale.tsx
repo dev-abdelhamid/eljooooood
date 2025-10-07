03ff892097239cc847fb49774771cfa50056685d
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-08 02:26:14 +0300

    one

--- src/pages/CreateSale.tsx
+++ src/pages/CreateSale.tsx
@@ -702,7 +702,7 @@ export const CreateSale: React.FC = () => {
   }, [cartState.items]);
 
   return (
-    <div className={`mx-auto px-4 sm:px-6 py-8 min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 font-sans ${isRtl ? 'font-arabic' : ''}`} dir={isRtl ? 'rtl' : 'ltr'}>
+    <div className={`mx-auto px-4  py-8 min-h-screen bg-gradient-to-br from-gray-50 to-gray-100  `}>
       <header className="mb-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-between sm:items-center">
         <div className="flex items-center gap-3">
           <DollarSign className="w-7 h-7 text-amber-600" />
