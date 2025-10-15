6a6bb3221eb5929887065b987d732260139a5e49
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-15 09:17:55 +0300

    one33932

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -218,13 +218,7 @@ export function Sidebar({
                     </motion.div>
                   )}
 
-                  {/* مؤشر النشاط */}
-                  {isActive && (
-                    <motion.div 
-                      className="absolute left-0 top-0 bottom-0 w-1 bg-gradient-to-b from-amber-400 to-amber-600"
-                      layoutId="active-indicator"
-                    />
-                  )}
+                 
                 </NavLink>
               ))}
             </nav>
