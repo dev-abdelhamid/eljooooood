b0bd20f36610ad8fac3d84cd3084c9777042596e
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-16 10:49:18 +0300

    one332

--- src/pages/OrdersTablePage.tsx
+++ src/pages/OrdersTablePage.tsx
@@ -307,7 +307,8 @@ const generatePDFTable = (
           let processedText = text;
           if (isRtl) {
             processedText = String(processedText).replace(/[0-9]/g, d => toArabicNumerals(d));
-            if (hookData.section === 'body') {              processedText = processedText.split('').reverse().join('');
+            if (processedText.match(/^[0-9+-%., ]*$/)) {
+              processedText = processedText.split('').reverse().join('');
             }
           }
           return processedText;
