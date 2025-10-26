deaf6051cd4f2df4a5b61402b0f0aa3cd75fb521
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-10-26 16:08:31 +0200

    one332

--- src/pages/SalesAnalytics.tsx
+++ src/pages/SalesAnalytics.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback, useMemo  , useRef} from 'react';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useAuth } from '../contexts/AuthContext';
 import { salesAPI, ordersAPI, returnsAPI, branchesAPI } from '../services/api';
