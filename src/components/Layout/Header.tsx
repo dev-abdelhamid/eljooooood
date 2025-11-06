3406d395f55dc410c925ae32e93a652b4e650333
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-11-06 15:10:21 +0200

    first commit

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -20,7 +20,7 @@ export function Header({ onMenuToggle }: HeaderProps) {
         <img
           src="/logo (3).png"
           alt={t('app.logo_alt')}
-          className="w-36 h-16 object-contain drop-shadow-xl"
+          className="w-36 h-16 object-contain drop-shadow-2xl    "
         />
     
       </div>
