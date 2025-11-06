e1bf2a7affabd37c124d0340763eb5f4bf0e4762
abdelhamid <abdelhamiddahi@std.mans.edu.eg>
2025-11-06 15:05:02 +0200

    first commit

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -20,7 +20,7 @@ export function Header({ onMenuToggle }: HeaderProps) {
         <img
           src="/logo (3).png"
           alt={t('app.logo_alt')}
-          className="w-20 h-10 sm:w-12 sm:h-12 rounded-xl border border-amber-300 shadow-sm"
+          className="w-32 h-10  rounded-xl border border-amber-300 shadow-sm"
         />
     
       </div>
